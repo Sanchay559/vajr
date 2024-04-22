59975d40bd9bf31415230f26cc46efea86aaa648
Sanchay559 <149278258+Sanchay559@users.noreply.github.com>
2024-04-23 04:05:48 +0530

    vajr.ino

--- vajr.ino
+++ vajr.ino
@@ -7,43 +7,34 @@
 boolean stringComplete = false;
 String inputString = "";
 String fromGSM = "";
-int c = 0;
-String SOS_NUM = "+917982860425";  // Your SOS number (Update as needed)
-
-int SOS_Time = 5;  // SOS button press duration in seconds
+String SOS_NUM = "+917982860425";  // Your SOS number
+int SOS_Time = 5000;  // SOS button press duration in milliseconds
 bool CALL_END = true;  // State flag for call end
 String res = "";  // Buffer for storing response from GSM module
 char response[50];  // Declare 'response' with a fixed-size char array
+unsigned long buttonPressStart = 0;  // Time when button was first pressed
+bool locationSent = false;  // Flag to avoid sending multiple times
 
 void setup() {
-  // Set WiFi and Bluetooth modes to off for power-saving
-  WiFi.mode(WIFI_OFF);  // Disable WiFi
+  WiFi.mode(WIFI_OFF);  // Disable WiFi for power saving
   btStop();  // Disable Bluetooth
 
-  // Set up pin modes
   pinMode(SOS, INPUT_PULLUP);  // SOS button with pull-up
   pinMode(SLEEP_PIN, OUTPUT);  // Sleep control pin
 
-  // Set up serial communication
-  Serial.begin(115200);  // For debugging via Serial Monitor
-  Serial2.begin(115200, SERIAL_8N1, 16, 17);  // UART interface for GSM communication (ESP32 RX: 16, TX: 17)
+  Serial.begin(115200);  // For debugging
+  Serial2.begin(115200, SERIAL_8N1, 16, 17);  // UART for GSM (ESP32 RX: 16, TX: 17)
 
-  // Allow time for GSM module initialization
-  delay(20000);
+  delay(20000);  // Time for GSM module initialization
+  digitalWrite(SLEEP_PIN, HIGH);  // Activate sleep mode
 
-  // Activate sleep mode
-  digitalWrite(SLEEP_PIN, HIGH);
-
-  // Basic AT commands to verify communication
+  // Check basic communication
   Serial2.println("AT");
   delay(1000);
-
   Serial2.println("AT+GPS=1");  // Enable GPS
   delay(1000);
-
   Serial2.println("AT+GPSLP=2");  // Set GPS to low power mode
   delay(1000);
-
   Serial2.println("AT+SLEEP=1");  // Configure sleep mode
   delay(1000);
 }
@@ -54,7 +45,6 @@ void loop() {
     char inChar = Serial2.read();
 
     if (inChar == '\n') {
-      // Check GSM module response
       if (fromGSM == "OK\r") {
         Serial.println("---------IT WORKS-------");
       } else if (fromGSM == "RING\r") {
@@ -69,7 +59,6 @@ void loop() {
         digitalWrite(SLEEP_PIN, HIGH);  // Re-enter sleep mode
       }
 
-      // Print GSM module response
       Serial.println(fromGSM);
       fromGSM = "";  // Clear the buffer
     } else {
@@ -83,45 +72,33 @@ void loop() {
     Serial2.write(inByte);
   }
 
-  // SOS button handling
-  if (digitalRead(SOS) == LOW && CALL_END) {
-    Serial.print("Calling in..");
-    for (c = 0; c < SOS_Time; c++) {
-      Serial.println(SOS_Time - c);  // Countdown for SOS button press
-      delay(1000);  // 1-second delay
-      if (digitalRead(SOS) == HIGH) {  // If button is released, break
-        break;
-      }
+  // Check SOS button press duration
+  if (digitalRead(SOS) == LOW) {
+    if (buttonPressStart == 0) {
+      // Start counting when button is pressed
+      buttonPressStart = millis();
     }
 
-    if (c == SOS_Time) {  // If SOS button held for 5 seconds
-      // Wake up and request GPS location
-      digitalWrite(SLEEP_PIN, LOW);  // Wake from sleep mode
+    if (millis() - buttonPressStart >= SOS_Time && !locationSent) {
+      // If the button is held for at least 5 seconds, send location
+      digitalWrite(SLEEP_PIN, LOW);  // Wake up
       delay(1000);
       Serial2.println("AT+LOCATION=2");  // Request GPS location
-      Serial.println("AT+LOCATION=2");
-
-      // Wait for response from GSM module
-      while (!Serial2.available()) {
-        delay(10);  // Small delay to avoid busy-waiting
-      }
+      delay(1000);
 
       res = "";
       while (Serial2.available()) {
         char add = Serial2.read();
-        res += add;  // Collect response
+        res += add;
       }
 
-      res = res.substring(17, 38);  // Extract location data
+      res = res.substring(17, 38);  // Extract GPS data
       strcpy(response, res.c_str());  // Convert to char array
 
       Serial.print("Received Data: ");
       Serial.println(response);
 
-      if (strstr(response, "GPS NOT")) {
-        Serial.println("No GPS data available");
-      } else {
-        // Parse GPS coordinates
+      if (!strstr(response, "GPS NOT")) {
         int i = 0;
         while (response[i] != ',') {
           i++;
@@ -131,32 +108,32 @@ void loop() {
         String lat = location.substring(2, i);  // Latitude
         String longi = location.substring(i + 1);  // Longitude
 
-        // Create Google Maps link
-        String Gmaps_link = "http://maps.google.com/maps?q=" + lat + "+" + longi;
+        String Gmaps_link = "http://maps.google.com/maps?q=" + lat + "+" + longi;  // Google Maps link
 
-        // Send SMS with Google Maps link
+        // Send SMS with location
         Serial2.println("AT+CMGF=1");  // Text mode
         delay(1000);
         Serial2.println("AT+CMGS=\"" + SOS_NUM + "\"\r");  // Prepare to send SMS
         delay(1000);
-
-        Serial2.println("I'm here: " + Gmaps_link);  // SMS content
+        Serial2.println("I'm here: " + Gmaps_link);  // Send SMS content
         delay(1000);
-        Serial2.println((char)26);  // End of SMS (Ctrl-Z)
+        Serial2.println((char)26);  // End of SMS
         delay(1000);
 
-        // Make a call after sending the SMS
-        Serial2.println("ATD" + SOS_NUM);  // Dial SOS number
-        CALL_END = false;
+        locationSent = true;  // Prevent multiple sends
       }
 
       res = "";  // Clear response buffer
     }
+  } else {
+    // Reset if the button is released
+    buttonPressStart = 0;
+    locationSent = false;
   }
 
   if (stringComplete) {
     Serial2.print(inputString);  // Send full message to GSM module
-    inputString = "" ;  // Reset input string
+    inputString = "";  // Reset input string
     stringComplete = false;
   }
 }
